eb32c1fa4c17b4cd529044f57566865b4ff5c94d
disha-h <2025cs_dishah_b@nie.ac.in>
2026-09-22 13:29:19 +0530

    path parameters with type hint

--- DAY/DAY2/FASTAPIPractice/main.py
+++ DAY/DAY2/FASTAPIPractice/main.py
@@ -10,3 +10,15 @@ def about():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+#POST request
+@app.post("/create")
+def create_something():
+    return {"message": "something created"}
+#path parameters
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"result":"Distinction","usn":usn}
+#path parameters with type hint
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno:int):
+    return {"result":"Distinction","rollno":rollno}
